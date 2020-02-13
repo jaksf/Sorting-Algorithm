f5b540a1365ce9e5ecba6faafae6dce23993d89c
jaksf <53540904+jaksf@users.noreply.github.com>
2020-02-13 22:24:38 +0000

    Updated Test Cases

--- src/SortComparisonTest.java
+++ src/SortComparisonTest.java
@@ -30,6 +30,19 @@ public class SortComparisonTest
     @Test
     public void testEmpty()
     {
+    	double array[] = null;
+    	SortComparison sorter = new SortComparison();
+    	
+    	double sortedArray[] = sorter.insertionSort(array);
+    	assertEquals(sortedArray, array);
+    	sortedArray = sorter.selectionSort(array);
+    	assertEquals(sortedArray, array);
+    	sortedArray = sorter.quickSort(array);
+    	assertEquals(sortedArray, array);
+    	sortedArray = sorter.mergeSortIterative(array);
+    	assertEquals(sortedArray, array);
+    	sortedArray = sorter.mergeSortRecursive(array);
+    	assertEquals(sortedArray, array);
     }
     
     
@@ -38,26 +51,53 @@ public class SortComparisonTest
     {
     }
     
+    @Test
     public void testSelection()
     {
     	
     }
     
+    @Test
     public void testQuick()
     {
     	
     }
     
+    @Test
     public void testMergeIncursive()
     {
     	
     }
     
+    @Test
     public void testMergeRecursive()
     {
     	
     }
 
+    public void printArray(double a[])
+    {
+    	String array = "";
+    	for(int i = 0; i < a.length; i++)
+    	{
+    		array += a[i];
+    		if (i != a.length-1)
+    		{
+    			array += ", ";
+    		}
+    		if (i % 20 == 19)
+    		{
+    			array += "\n";
+    		}
+    	}
+    	System.out.println("Array: " + array);
+    }
+    
+    public double[] importArray(String file)
+    {
+    	double a[] = null;
+    	return a;
+    }
 
     // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
     // be executed at least once from at least one test.
