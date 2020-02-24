fa7368a966bba117f0b42160dc08e9651462cadb
jaksf <53540904+jaksf@users.noreply.github.com>
2020-02-24 20:28:31 +0000

    Input files added, more tests

--- src/SortComparison.java
+++ src/SortComparison.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 // -------------------------------------------------------------------------
 
 /**
@@ -8,7 +10,7 @@
  *  @version HT 2020
  */
 
- class SortComparison {
+ class SortComparison{
 
     /**
      * Sorts an array of doubles using InsertionSort.
@@ -18,21 +20,21 @@
      *
      */
     static double [] insertionSort (double a[]){
-
+    	double [] array = a.clone();
         for(int i = 1; i < a.length; i++)
-        {
-        double key = a[i];
+       {
+        double key = array[i];
         int j = i - 1;
         
-        while(j >= 0 && a[j] > key)
-        {
-        	a[j+1] = a[j];
+        while(j >= 0 && array[j] > key)
+       {
+        	array[j+1] = array[j];
         	j = j -1;
         }
-        a[j+1] = key;
+        array[j+1] = key;
         }
-        return a;
-    }//end insertionsort
+        return array;
+    }
 	
 	    /**
      * Sorts an array of doubles using Selection Sort.
@@ -42,20 +44,23 @@
      *
      */
     static double [] selectionSort (double a[]){
-
-    	int n = a.length; 
+    	double [] array = a.clone();
+    	int n = array.length; 
         for (int i = 0; i < n-1; i++) 
-        { 
+       {
             int min = i; 
             for (int j = i+1; j < n; j++)
-            {
-            	if (a[j] < a[min]){ min = j;} 
-            } 
-            double temp = a[min]; 
-            a[min] = a[i]; 
-            a[i] = temp; 
+           {
+            	if (array[j] < array[min])
+            	{
+            		min = j;
+            	}
+            }
+            double temp = array[min]; 
+            array[min] = array[i]; 
+            array[i] = temp; 
         }
-    	return a;
+    	return array;
     }//end selectionsort
 
     /**
@@ -65,12 +70,44 @@
      * @return array sorted in ascending order
      *
      */
-    static double [] quickSort (double a[]){
-	
-		 //todo: implement the sort
-    	return null;
-    }//end quicksort
-
+    static double [] quickSort (double a[])
+   {
+    	double [] array = a.clone();
+    	quickSorter(array, 0, array.length - 1);
+    	return array;
+    }
+    
+    private static void quickSorter(double a[], int low, int high)
+   {
+    	if (low < high) 
+       {
+            int pi = quickSortPartition(a, low, high); 
+            quickSorter(a, low, pi-1); 
+            quickSorter(a, pi+1, high); 
+        }
+    }
+    
+    private static int quickSortPartition(double a[], int low, int high)
+   {
+    	double pivot = a[high];  
+        int i = (low-1);
+        for (int j = low; j < high; j++) 
+       {
+            if (a[j] < pivot) 
+           {
+                i++; 
+                double temp1 = a[i]; 
+                a[i] = a[j]; 
+                a[j] = temp1; 
+            }
+        }
+        double temp2 = a[i+1]; 
+        a[i+1] = a[high]; 
+        a[high] = temp2; 
+  
+        return i+1; 
+    }
+    
     /**
      * Sorts an array of doubles using Merge Sort.
      * This method is static, thus it can be called as SortComparison.sort(a)
@@ -86,13 +123,45 @@
      * @return after the method returns, the array must be in ascending sorted order.
      */
 
-    static double[] mergeSortIterative (double a[]) {
+    static double[] mergeSortIterative (double a[])
+    {
+    	double [] array = a.clone();
+    	int low = 0;
+		int high = array.length - 1;
 
-		 //todo: implement the sort
-    	return null;
-	
-    }//end mergesortIterative
+		double[] temp = Arrays.copyOf(array, array.length);
+		for (int m = 1; m <= high - low; m = 2*m)
+		{
+			for (int i = low; i < high; i += 2*m)
+			{
+				int from = i;
+				int mid = i + m - 1;
+				int to = Integer.min(i + 2 * m - 1, high);
+
+				mergeSortIterativeMerge(array, temp, from, mid, to);
+			}
+		}
+    	return array;
+    }
     
+    static private void mergeSortIterativeMerge(double[] a, double[] temp, int from, int mid, int to)
+    {
+    	int k = from, i = from, j = mid + 1;
+		while (i <= mid && j <= to) {
+			if (a[i] < a[j]) {
+				temp[k++] = a[i++];
+			} else {
+				temp[k++] = a[j++];
+			}
+		}
+		while (i <= mid) {
+			temp[k++] = a[i++];
+		}
+		for (i = from; i <= to; i++) {
+			a[i] = temp[i];
+		}
+    	return;
+    }
     
     
     /**
@@ -102,21 +171,101 @@
      * @param a: An unsorted array of doubles.
      * @return after the method returns, the array must be in ascending sorted order.
      */
-    static double[] mergeSortRecursive (double a[]) {
-		return null;
-    	
-
-    	//todo: implement the sort
-	
-   }//end mergeSortRecursive
-    	
+    public static double[] mergeSortRecursive (double a[])
+   {
+    	double[] array = a.clone();
+    	mergeSortRecursiveSorter(array, 0, array.length-1);
+    	return array;
+   }
     
-
-
+   private static double[] mergeSortRecursiveSorter (double a[], int left, int right)
+   {
+	   if (left < right) 
+       { 
+           // Find the middle point 
+           int mid = (left + right)/2; 
+ 
+           // Sort first and second halves 
+           mergeSortRecursiveSorter(a, left, mid); 
+           mergeSortRecursiveSorter(a , mid + 1, right); 
+ 
+           // Merge the sorted halves 
+           mergeSortRecursiveMerger(a, left, mid, right); 
+       } 
+	   
+	   return a;
+   }
    
+   private static void mergeSortRecursiveMerger (double a[], int l, int m, int r)
+   {
+	// Find sizes of two subarrays to be merged 
+       int n1 = m - l + 1; 
+       int n2 = r - m; 
+ 
+       /* Create temp arrays */
+       double leftArray[] = new double [n1]; 
+       double rightArray[] = new double [n2]; 
+ 
+       /*Copy data to temp arrays*/
+       for (int i=0; i<n1; ++i) 
+           leftArray[i] = a[l + i]; 
+       for (int j=0; j<n2; ++j) 
+           rightArray[j] = a[m + 1+ j]; 
+ 
+ 
+       /* Merge the temp arrays */
+ 
+       // Initial indexes of first and second subarrays 
+       int i = 0, j = 0; 
+ 
+       // Initial index of merged subarry array 
+       int k = l; 
+       while (i < n1 && j < n2) 
+       { 
+           if (leftArray[i] <= rightArray[j]) 
+           { 
+               a[k] = leftArray[i]; 
+               i++; 
+           } 
+           else
+           { 
+               a[k] = rightArray[j]; 
+               j++; 
+           } 
+           k++; 
+       } 
+ 
+       /* Copy remaining elements of L[] if any */
+       while (i < n1) 
+       { 
+           a[k] = leftArray[i]; 
+           i++; 
+           k++; 
+       } 
+ 
+       /* Copy remaining elements of R[] if any */
+       while (j < n2) 
+       { 
+           a[k] = rightArray[j]; 
+           j++; 
+           k++; 
+       } 
+   }
+    
+   public static boolean isSorted(double a[])
+   {
+	   for(int i = 1; i < a.length; i++)
+	   {
+		   if(a[i] < a[i-1])
+		   {
+			   return false;
+		   }
+	   }
+	   return true;
+   }
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         //todo: do experiments as per assignment instructions
     }
--- src/SortComparisonTest.java
+++ src/SortComparisonTest.java
@@ -1,4 +1,10 @@
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.Scanner;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -30,76 +36,171 @@ public class SortComparisonTest
     @Test
     public void testEmpty()
     {
-    	double array[] = null;
     	SortComparison sorter = new SortComparison();
-    	
-    	double sortedArray[] = sorter.insertionSort(array);
-    	assertEquals(sortedArray, array);
-    	sortedArray = sorter.selectionSort(array);
-    	assertEquals(sortedArray, array);
-    	sortedArray = sorter.quickSort(array);
-    	assertEquals(sortedArray, array);
-    	sortedArray = sorter.mergeSortIterative(array);
-    	assertEquals(sortedArray, array);
-    	sortedArray = sorter.mergeSortRecursive(array);
-    	assertEquals(sortedArray, array);
+    	double[] ordered = {};
+    	double[] orderedInsertion = sorter.insertionSort(ordered);
+    	double[] orderedSelection = sorter.selectionSort(ordered);
+    	double[] orderedQuick = sorter.quickSort(ordered);
+    	double[] orderedMergeSortIterative = sorter.mergeSortIterative(ordered);
+    	double[] orderedMergeSortRecursive = sorter.mergeSortRecursive(ordered);
+    	assertEquals(sorter.isSorted(orderedInsertion), true);
+    	assertEquals(sorter.isSorted(orderedSelection), true);
+    	assertEquals(sorter.isSorted(orderedQuick), true);
+    	assertEquals(sorter.isSorted(orderedMergeSortIterative), true);
+    	assertEquals(sorter.isSorted(orderedMergeSortRecursive), true);
+    }
+    
+    @Test
+    public void testOrdered()
+    {
+    	SortComparison sorter = new SortComparison();
+    	double[] ordered = {1,2,3,4,5};
+    	double[] orderedInsertion = sorter.insertionSort(ordered);
+    	double[] orderedSelection = sorter.selectionSort(ordered);
+    	double[] orderedQuick = sorter.quickSort(ordered);
+    	double[] orderedMergeSortIterative = sorter.mergeSortIterative(ordered);
+    	double[] orderedMergeSortRecursive = sorter.mergeSortRecursive(ordered);
+    	assertEquals(sorter.isSorted(orderedInsertion), true);
+    	assertEquals(sorter.isSorted(orderedSelection), true);
+    	assertEquals(sorter.isSorted(orderedQuick), true);
+    	assertEquals(sorter.isSorted(orderedMergeSortIterative), true);
+    	assertEquals(sorter.isSorted(orderedMergeSortRecursive), true);
+    }
+    
+    @Test
+    public void testReverseOrdered()
+    {
+    	SortComparison sorter = new SortComparison();
+    	double[] ordered = {5,4,3,2,1};
+    	double[] orderedInsertion = sorter.insertionSort(ordered);
+    	double[] orderedSelection = sorter.selectionSort(ordered);
+    	double[] orderedQuick = sorter.quickSort(ordered);
+    	double[] orderedMergeSortIterative = sorter.mergeSortIterative(ordered);
+    	double[] orderedMergeSortRecursive = sorter.mergeSortRecursive(ordered);
+    	assertEquals(sorter.isSorted(orderedInsertion), true);
+    	assertEquals(sorter.isSorted(orderedSelection), true);
+    	assertEquals(sorter.isSorted(orderedQuick), true);
+    	assertEquals(sorter.isSorted(orderedMergeSortIterative), true);
+    	assertEquals(sorter.isSorted(orderedMergeSortRecursive), true);
+    }
+    
+    @Test
+    public void testUnsorted()
+    {
+    	SortComparison sorter = new SortComparison();
+    	double[] a = {2,2,1,4};
+    	assertEquals(sorter.isSorted(a), false);
+    }
+    
+    @Test
+    public void testSorted()
+    {
+    	SortComparison sorter = new SortComparison();
+    	double[] a = {1,2,3,5};
+    	assertEquals(sorter.isSorted(a), true);
     }
     
     
     @Test
     public void testInsertion()
     {
+    	SortComparison sorter = new SortComparison();
+    	double[] unordered = {5,3,2,8,9,4,0,0,3};
+    	double[] orderedInsertion = sorter.insertionSort(unordered);
+    	assertEquals(sorter.isSorted(orderedInsertion), true);
     }
     
     @Test
     public void testSelection()
     {
-    	
+    	SortComparison sorter = new SortComparison();
+    	double[] unordered = {5,3,2,8,9,4,0,0,3};
+    	double[] orderedSelection = sorter.selectionSort(unordered);
+    	assertEquals(sorter.isSorted(orderedSelection), true);
     }
     
     @Test
     public void testQuick()
     {
-    	
+    	SortComparison sorter = new SortComparison();
+    	double[] unordered = {5,3,2,8,9,4,0,0,3};
+    	double[] orderedQuick = sorter.quickSort(unordered);
+    	assertEquals(sorter.isSorted(orderedQuick), true);
     }
     
     @Test
     public void testMergeIncursive()
     {
-    	
+    	SortComparison sorter = new SortComparison();
+    	double[] unordered = {5,3,2,8,9,4,0,0,3};
+    	double[] orderedMergeIterative = sorter.mergeSortIterative(unordered);
+    	assertEquals(sorter.isSorted(orderedMergeIterative), true);
     }
     
     @Test
     public void testMergeRecursive()
     {
-    	
+    	SortComparison sorter = new SortComparison();
+    	double[] unordered = {5,3,2,8,9,4,0,0,3};
+    	double[] orderedMergeRecursive = sorter.mergeSortRecursive(unordered);
+    	assertEquals(sorter.isSorted(orderedMergeRecursive), true);
     }
-
-    public void printArray(double a[])
-    {
-    	String array = "";
-    	for(int i = 0; i < a.length; i++)
-    	{
-    		array += a[i];
-    		if (i != a.length-1)
-    		{
-    			array += ", ";
-    		}
-    		if (i % 20 == 19)
-    		{
-    			array += "\n";
-    		}
-    	}
-    	System.out.println("Array: " + array);
-    }
+//    @Test
+//    public void testImportedArraySort()
+//    {
+//    	SortComparison sorter = new SortComparison();
+//    	double[] ordered = {};
+//		ordered = importArray("numbers100000.txt", 100000);
+//    	double[] orderedInsertion = sorter.insertionSort(ordered);
+//    	double[] orderedSelection = sorter.selectionSort(ordered);
+//    	double[] orderedQuick = sorter.quickSort(ordered);
+//    	double[] orderedMergeSortIterative = sorter.mergeSortIterative(ordered);
+//    	double[] orderedMergeSortRecursive = sorter.mergeSortRecursive(ordered);
+//    	assertEquals(sorter.isSorted(orderedInsertion), true);
+//    	assertEquals(sorter.isSorted(orderedSelection), true);
+//    	assertEquals(sorter.isSorted(orderedQuick), true);
+//    	assertEquals(sorter.isSorted(orderedMergeSortIterative), true);
+//    	assertEquals(sorter.isSorted(orderedMergeSortRecursive), true);
+//    }
     
-    public double[] importArray(String file)
-    {
-    	double a[] = null;
-    	return a;
-    }
+//    public void printArray(double a[])
+//    {
+//    	String array = "";
+//    	for(int i = 0; i < a.length; i++)
+//    	{
+//    		array += a[i];
+//    		if (i != a.length-1)
+//    		{
+//    			array += ", ";
+//    		}
+//    		if (i % 20 == 19)
+//    		{
+//    			array += "\n";
+//    		}
+//    	}
+//    	System.out.println("Array: " + array);
+//    }
+    
+//    public double[] importArray(String fileLocation, int size)
+//    {
+//    	File file = new File(fileLocation);
+//    	Scanner scanner = null;
+//		try {
+//			scanner = new Scanner(file);
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//    	double[] array = new double[size];
+//    	int i =0;
+//    	while(scanner.hasNextDouble())
+//    	{
+//    		   array[i++] = scanner.nextInt();
+//    	}
+//    	return array;
+//    }
 
-    // TODO: add more tests here. Each line of code and ech decision in Collinear.java should
+    // TODO: add more tests here. Each line of code and each decision in Collinear.java should
     // be executed at least once from at least one test.
 
     // ----------------------------------------------------------
